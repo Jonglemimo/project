71363bb601ef4175cb8618d8c7bf919dc7873d21
Pierre Roland <pierrer1995@hotmail.fr>
2016-12-20 11:54:00 +0100

    ajout bootstrap jquery cdn

--- w/app/Views/layout.php
+++ w/app/Views/layout.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
-
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
 <body>
@@ -19,5 +19,8 @@
 		<footer>
 		</footer>
 	</div>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="<?= $this->assetUrl('js/script.js') ?>"></script>
 </body>
 </html>
