b71e1b247bff6a6364de9043ac8daf00cd48d42a
Victor Tarrieu <victor.tarrieu@yahoo.fr>
2016-12-20 14:41:03 +0100

    ajout du gitignore pour le dossier .idea

--- w/app/Controller/UserController.php
+++ w/app/Controller/UserController.php
@@ -1,7 +1,156 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Helda
- * Date: 20/12/2016
- * Time: 13:50
- */
+
+namespace Controller;
+
+use W\Controller\Controller;
+use W\Model\UsersModel;
+
+class UserController extends Controller
+{
+
+    public function signin()
+    {
+
+        if (isset($_POST['signin'])) {
+
+            $authModel = new AuthentificationModel();
+            $userModel = new UsersModel();
+            $errors = array();
+
+            //vérification du pseudo/mail
+
+            if (empty($_POST['emailorPseudo'])) {
+                $errors['email'] = true;
+            } else {
+                $emailorPseudo = trim($_POST['emailorPseudo']);
+            }
+
+            //vérification du password
+
+            if (empty($_POST['password'])) {
+                $errors['password'] = true;
+            } else {
+                $password = trim($_POST['password']);
+            }
+
+            //vérification on vérifie qu'il existe en base de donné
+
+            $userId = $authModel->isValidLoginInfo($emailorPseudo, $password);
+
+            if($userId == 0){
+                $errors['login']['wrong'] = true;
+            }
+
+            if ($userId > 0 && count($errors == 0)) {
+
+                // Connexion
+                $user = $userModel->find($userId);
+
+                // Placer user en session : $_SESSION['user'] = $user
+                $authModel->logUserIn($user);
+                $this->redirectToRoute('default_home');
+            } else {
+                // Echec de la connexion
+                $errors['echec'] = true;
+                $this->show('user/login', ['errors' => $errors]);
+            }
+        } else {
+            $this->show('user/signin');
+
+        }
+    }
+
+    public function signup ()
+    {
+        if(isset($_POST['signup'])) {
+
+            $userModel = new UsersModel();
+            $authModel = new AuthentificationModel();
+            $errors = array();
+
+            //vérification du pseudo
+
+            if (empty($_POST['pseudo'])) {
+                $errors['pseudo']['empty'] = true;
+            } else {
+                $pseudo = trim($_POST['pseudo']);
+                $pseudo = htmlspecialchars($pseudo, ENT_QUOTES);
+
+                if ($userModel->usernameExists($_POST['pseudo'])) {
+                    $errors['pseudo']['exist'] = true;
+                }
+            }
+
+            //vérification firstname
+
+            if (empty($_POST['firstname'])) {
+                $errors['firstname']['empty'] = true;
+
+            } elseif (strlen($_POST['firstname']) < 5) {
+                $errors['firstname']['short'] = true;
+            } else {
+                $firstname = $_POST['firstname'];
+            }
+
+            //vérification lastname
+
+            if (empty($_POST['lastname'])) {
+                $errors['lastname']['empty'] = true;
+
+            } elseif (strlen($_POST['lastname']) < 5) {
+                $errors['lastname']['short'] = true;
+            } else {
+                $lastname = $_POST['lastname'];
+            }
+
+            //vérification password
+
+            if (empty($_POST['password'])) {
+                $errors['password']['empty'] = true;
+
+            } elseif (strlen($_POST['password']) < 5) {
+                $errors['password']['short'] = true;
+            } else {
+                $password = $_POST['password'];
+            }
+
+            //vérification email
+
+            if (empty($_POST['email'])) {
+                $errors['email']['empty'] = true;
+
+            } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors['email']['wrong'] = true;
+
+            } else {
+                $email = $_POST['email'];
+                if($userModel->emailExists($email)){
+                    $errors['email']['exist'] = true;
+                }
+            }
+
+            //si aucune erreur, on ajoute en BDD
+
+            if(count($errors > 0)){
+
+                $password = $authModel -> hashPassword($password,PASSWORD_DEFAULT);
+                $userModel ->insert([
+                    'firstname'=> $firstname,
+                    'lastname' => $lastname,
+                    'email'    => $email,
+                    'pseudo' => $pseudo,
+                    'password' => $password,
+
+
+                ]);
+
+                // On redirige sur une page sucess afin d'afficher les informations du compte de l'utilisateur
+
+                $this->redirect('user/sucess');
+            }
+
+        }else {
+            $this->show('user/signup');
+        }
+    }
+}
--- w/app/routes.php
+++ w/app/routes.php
@@ -1,5 +1,9 @@
 <?php
 	
 	$w_routes = array(
-		['GET', '/', 'Default#home', 'default_home'],
+		['GET',                 '/',                                'Default#home',         'default_home'],
+
+        ['GET|POST',			'/signin',							'User#login',			'user_login'],
+        ['GET|POST',			'/signup',							'User#signup',			'user_signup'],
+
 	);
