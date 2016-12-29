e3a27a691f839a796312901a8592f837c384c43f
Victor Tarrieu <victor.tarrieu@yahoo.fr>
2016-12-29 22:30:37 +0100

    suppression fichier obsolètes

--- backup/w/app/Views/default/categories.php
+++
@@ -1,4 +0,0 @@
-<?php
-foreach ($categories as $category):?>
-    <li><a href="<?=$this->url('category_videos',['name' => $category['name']])?>"><?=$category['name']?></a></li>
-<?php endforeach; ?>
